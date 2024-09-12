e9f1606aa1a3f7040a812f7416bb63b47aa30bdf
Попова Дарья Алексеевна <pl1295@pl1.edu>
2024-09-12 10:04:48 +0500

    12.09 changing

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+//echo вывод привет мир
+echo 5+5;
+echo "<br>";
+echo 5 . " степеней " . 65 . " степеней ";
+$a = 5;
+$b = "0123456789";
+
+echo $a + $b;
+
+echo "<br>";
+const A = 54;
+
+echo A;
+
+echo "$a + $b";
+echo "<br>";
+echo $b[6];
+
+?>
